16fff0bcb309560bec2f01a83c03de55d0e047e7
ghbhaha <ghbhaha@gmail.com>
2015-01-12 21:06:59 +0800

    SudaMod bringup
    Change-Id: I6e116c9b3860fcfa8005abb45368ef9c116e30ac

--- envsetup.sh
+++ envsetup.sh
@@ -732,7 +732,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var SM_VERSION)
-        ZIPFILE=sm-$MODVERSION.zip
+        ZIPFILE=$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
